6202b1e187c8500f20af58fa50eb2da5b9001191
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-12 08:29:15 -0500

    add: infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,19 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlPage = `&page=${page}`;
+    url = `${mainUrl}${clientID}${urlPage}`;
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      setPhotos(oldPhotos => {
+        return [...oldPhotos, ...data];
+      });
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -27,7 +31,7 @@ function App() {
 
   useEffect(() => {
     fetchImages();
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     const event = window.addEventListener('scroll', () => {
@@ -35,7 +39,9 @@ function App() {
         !loading &&
         window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
       ) {
-        console.log('it worked');
+        setPage(oldPage => {
+          return oldPage + 1;
+        });
       }
     });
     return () => window.removeEventListener('scroll', event);
