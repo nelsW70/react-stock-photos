7f3605d9c5b65d42466965b646da677457587961
Nelson Foltz <nelsfoltz@gmail.com>
2020-11-12 16:37:45 -0500

    add: setPage on handleSubmit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
   const [query, setQuery] = useState('');
 
   const fetchImages = async () => {
@@ -27,9 +27,10 @@ function App() {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
       setPhotos(oldPhotos => {
-        if (query) {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
           return [...oldPhotos, ...data.results];
         } else {
           return [...oldPhotos, ...data];
@@ -62,7 +63,7 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    fetchImages();
+    setPage(1);
   };
 
   return (
